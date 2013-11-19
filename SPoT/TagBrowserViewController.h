16368e71ebf4058bec3a638a56eb0216d304d48b
Michel Mansour <michel.mansour@gmail.com>
2013-11-19 00:44:11 -0500

    renamed from BrowseViewController; finished implementation of prepareForSegue to photo list

---
+++ SPoT/TagBrowserViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowseViewController.h
+//  SPoT
+//
+//  Created by Michel Mansour on 11/18/13.
+//  Copyright (c) 2013 Michel Mansour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TagBrowserViewController : UITableViewController
+
+@end
---
+++ SPoT/TagBrowserViewController.m
@@ -0,0 +1,80 @@
+//
+//  TagBrowseViewController.m
+//  SPoT
+//
+//  Created by Michel Mansour on 11/18/13.
+//  Copyright (c) 2013 Michel Mansour. All rights reserved.
+//
+
+#import "TagBrowserViewController.h"
+#import "FlickrFetcher.h"
+
+@interface TagBrowserViewController ()
+@property (strong, nonatomic) NSMutableDictionary *tags;
+@property (strong, nonatomic) NSMutableArray *tagNames;
+@end
+
+@implementation TagBrowserViewController
+
+- (NSMutableDictionary *)tags {
+    if (!_tags) self.tags = [[NSMutableDictionary alloc] init];
+    return _tags;
+}
+
+- (NSMutableArray *)tagNames {
+    if (!_tagNames) self.tagNames = [[NSMutableArray alloc] init];
+    return _tagNames;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSArray *photos = [FlickrFetcher stanfordPhotos];
+    
+    NSArray *badTags = @[@"cs193pspot", @"portrait", @"landscape"];
+    for (NSDictionary *photo in photos) {
+        for (NSString *tag in [photo[FLICKR_TAGS] componentsSeparatedByString:@" "]) {
+            if (![badTags containsObject:tag]) {
+                NSString *capTag = [tag capitalizedString];
+                if (!self.tags[capTag]) {
+                    self.tags[capTag] = [[NSMutableArray alloc] init];
+                }
+                [self.tags[capTag] addObject:photo];
+            }
+        }
+    }
+    
+    [self.tagNames removeAllObjects];
+    [self.tagNames addObjectsFromArray:[self.tags allKeys]];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.tags count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"FlickrTag" forIndexPath:indexPath];
+    
+    NSString *tagTitle = self.tagNames[indexPath.row];
+    cell.textLabel.text = tagTitle;
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"%d", [self.tags[tagTitle] count]];
+    
+    return cell;
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([sender isKindOfClass:[UITableViewCell class]]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        if (indexPath) {
+            if ([segue.identifier isEqualToString:@"List Photos"]) {
+                if ([segue.destinationViewController respondsToSelector:@selector(setPhotos:)]) {
+                    [segue.destinationViewController performSelector:@selector(setPhotos:) withObject:self.tags[self.tagNames[indexPath.row]]];
+                    [segue.destinationViewController setTitle:self.tagNames[indexPath.row]];
+//                    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:self.tagNames[indexPath.row] style:UIBarButtonItemStyleBordered target:nil action:nil];
+//                    [segue.destinationViewController setBackBarButtonItem:backButton];
+                }
+            }
+        }
+    }
+}
+
+@end
