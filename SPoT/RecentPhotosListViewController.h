450ee762bf311c5b92845e0a3b327ffa063ba20b
Michel Mansour <michel.mansour@gmail.com>
2013-11-21 00:32:42 -0500

    added functionality for storing most recently viewed photos

---
+++ SPoT/RecentPhotosListViewController.h
@@ -0,0 +1,14 @@
+//
+//  RecentPhotosListViewController.h
+//  SPoT
+//
+//  Created by Michel Mansour on 11/20/13.
+//  Copyright (c) 2013 Michel Mansour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhotoListViewController.h"
+
+@interface RecentPhotosListViewController : PhotoListViewController
+
+@end
---
+++ SPoT/RecentPhotosListViewController.m
@@ -0,0 +1,49 @@
+//
+//  RecentPhotosListViewController.m
+//  SPoT
+//
+//  Created by Michel Mansour on 11/20/13.
+//  Copyright (c) 2013 Michel Mansour. All rights reserved.
+//
+
+#import "RecentPhotosListViewController.h"
+#import "RecentPhoto.h"
+
+@interface RecentPhotosListViewController ()
+
+@end
+
+@implementation RecentPhotosListViewController
+
+- (NSString *)reusableCellIdentifier {
+    return @"Recent Photo";
+}
+
+- (void)setup {
+    NSArray *recentPhotos = [RecentPhoto allRecentPhotosByLastViewed];
+    NSMutableArray *photos = [[NSMutableArray alloc] init];
+    for (id item in recentPhotos) {
+        if ([item isKindOfClass:[RecentPhoto class]]) {
+            RecentPhoto *recentPhoto = (RecentPhoto *)item;
+            [photos addObject:recentPhoto.photo];
+        }
+    }
+    [self setPhotos:photos];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setup];
+}
+
+- (void)awakeFromNib {
+    [self setup];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    [self setup];
+    return self;
+}
+
+@end
